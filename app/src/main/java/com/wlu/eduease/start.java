6a1e35e04e3452228f3b027fa39e97e83e474219
perin2610 <perinpatel0254@outlook.com>
2024-06-17 18:21:11 -0400

    change in start activity

--- app/src/main/java/com/wlu/eduease/start.java
+++ app/src/main/java/com/wlu/eduease/start.java
@@ -1,6 +1,9 @@
 package com.wlu.eduease;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class start extends AppCompatActivity {
 
+    Button buttonLogin, buttonRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start);
+
+        buttonLogin = findViewById(R.id.btn_login);
+        buttonRegister = findViewById(R.id.btn_register);
+
+
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(start.this, user_login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(start.this, user_register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
 
     }
 }
