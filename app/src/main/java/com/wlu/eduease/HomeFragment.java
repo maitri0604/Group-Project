1f42de66581894e2b2d3475c9f51dad85bfb4d7d
perin2610 <perinpatel0254@outlook.com>
2024-06-30 18:05:48 -0400

    change in start activity

---
+++ app/src/main/java/com/wlu/eduease/HomeFragment.java
@@ -0,0 +1,57 @@
+package com.wlu.eduease;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeFragment extends Fragment {
+
+    private FirebaseAuth auth;
+    private Button button;
+    private TextView textView;
+    private FirebaseUser user;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        auth = FirebaseAuth.getInstance();
+        button = view.findViewById(R.id.btn_logout);
+        textView = view.findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+
+        if (user == null) {
+            Intent intent = new Intent(getActivity(), user_login.class);
+            startActivity(intent);
+            getActivity().finish(); // Optional: finish the current activity if navigating away
+        } else {
+            textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), user_login.class);
+                startActivity(intent);
+                getActivity().finish(); // Optional: finish the current activity if navigating away
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/wlu/eduease/MainActivity.java
+++ app/src/main/java/com/wlu/eduease/MainActivity.java
@@ -1,53 +1,121 @@
 package com.wlu.eduease;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+    private DrawerLayout drawerLayout;
+    private FirebaseAuth auth;
+    private TextView textView;
+    private FirebaseUser user;
+    private DatabaseReference usersRef; // Reference to users node in Realtime Database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.btn_logout);
-        textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
+
         if (user == null) {
             Intent intent = new Intent(MainActivity.this, user_login.class);
             startActivity(intent);
             finish();
+        } else {
+            // Set user name in navigation drawer header
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+            textView = navigationView.getHeaderView(0).findViewById(R.id.user_details);
+
+            // Initialize Firebase Database reference
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            usersRef = database.getReference("users").child(user.getUid());
+
+            // Retrieve and set full name from database
+            usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        String fullName = dataSnapshot.child("fullname").getValue(String.class);
+                        textView.setText("Welcome " + fullName +" !");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(MainActivity.this, "Failed to load user data.", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
-        else {
-            textView.setText(user.getEmail());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        } else if (itemId == R.id.nav_settings) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+        } else if (itemId == R.id.nav_about) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
+        } else if (itemId == R.id.nav_logout) {
+            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+            // Perform logout actions here
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(MainActivity.this, user_login.class);
+            startActivity(intent);
+            finish();
         }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(MainActivity.this, user_login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
-
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
